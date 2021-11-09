b26b9e18595ca98659dc7fcb59bf14b15bf91da7
WSOMNATH <93254275+WSOMNATH@users.noreply.github.com>
2021-11-09 17:42:39 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,24 @@
+package genericClasses;
+
+import java.util.HashSet;
+
+public class Employee {
+	 public static Object set;
+	int id;
+	String name;
+ String department;
+	 Long salary;
+	
+	public Employee(int id, String name, String department, Long salary) {
+		//super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+		this.salary = salary;
+	//	displaydetails();
+	}
+	
+	
+	
+
+}
---
+++ hashSet.java
@@ -0,0 +1,33 @@
+package genericClasses;
+
+import java.util.*;
+
+public class hashSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Employee a=new Employee();
+		Employee e=new Employee(234,"Bhartesh","CSE",22000L);
+		Employee e2=new Employee(235,"Prajwal","CSE",32000L);
+		Employee e3=new Employee(343,"doms","string",57678989L);
+		HashSet<Employee> hash=new HashSet<Employee>();
+
+	hash.add(e);
+	hash.add(e2);
+    hash.add(e3);
+		/*a.setHash(hash);
+		hash.add(b);
+		b.setHash(hash);
+		hash.add(c);
+		c.setHash(hash);
+		hash.add(d);
+		d.setHash(hash);*/
+
+		for(Employee e1:hash) {
+			System.out.println(e1.id+" "+e1.name+" "+e1.department+" "+e1.salary);
+		}
+		
+		
+	}
+
+}
